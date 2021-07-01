d648c956982edb431e8a56cc2f2d47848714e858
github-action-benchmark <github@users.noreply.github.com>
2021-07-01 12:41:39 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for d3e25a2aa44c60401421606ffa31b371c5f24e12

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1620209752075,
-  "repoUrl": "https://github.com/prisma/prisma",
+  "lastUpdate": 1625143299514,
+  "repoUrl": "https://github.com/TheRakeshPurohit/prisma",
   "entries": {
     "Benchmark.js Benchmark": [
       {
@@ -18719,6 +18719,86 @@ window.BENCHMARK_DATA = {
           {
             "name": ".prisma/client/index.js size",
             "value": 2.5,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          },
+          {
+            "name": ".prisma/client/query-engine-debian-openssl-1.1.x size",
+            "value": 41,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          },
+          {
+            "name": "dotPlusAtPrismaClientFolder.zip size",
+            "value": 16,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pantharshit00@gmail.com",
+            "name": "Harshit Pant",
+            "username": "pantharshit00"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d3e25a2aa44c60401421606ffa31b371c5f24e12",
+          "message": "ci: action for updating engines via workflow dispatch (#7640)\n\nCo-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>\r\nCo-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>",
+          "timestamp": "2021-07-01T14:11:19+02:00",
+          "tree_id": "929442b8ad73c02029eb0f2e8e9ff21f5daeb697",
+          "url": "https://github.com/TheRakeshPurohit/prisma/commit/d3e25a2aa44c60401421606ffa31b371c5f24e12"
+        },
+        "date": 1625143297039,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "client generation ~50 Models",
+            "value": 0.44,
+            "range": "±4.99%",
+            "unit": "ops/sec",
+            "extra": "7 samples"
+          },
+          {
+            "name": "typescript compilation ~50 Models",
+            "value": 0.12,
+            "range": "±1.91%",
+            "unit": "ops/sec",
+            "extra": "5 samples"
+          },
+          {
+            "name": "@prisma/client size",
+            "value": 6.9,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          },
+          {
+            "name": ".prisma/client size",
+            "value": 46,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          },
+          {
+            "name": ".prisma/client/index.d.ts size",
+            "value": 1.7,
+            "range": "±0.00%",
+            "unit": "MB",
+            "extra": "1 samples"
+          },
+          {
+            "name": ".prisma/client/index.js size",
+            "value": 2.7,
             "range": "±0.00%",
             "unit": "MB",
             "extra": "1 samples"
